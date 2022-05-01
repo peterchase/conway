365227dd565ea864e815bd7ea614137e820babfb
Peter Chase <pete@peterchase.me>
2022-05-01 11:25:20 +0100

    Move console printout to separate class

--- conwayconsole/Program.cs
+++ conwayconsole/Program.cs
@@ -1,33 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using ConwayLib;
 
 namespace ConwayConsole
 {
+
     public static class Program
     {
-        private static readonly Dictionary<int, int> sColours = new Dictionary<int, int>
-        {
-            { 0, 34 },
-            { 1, 32 },
-            { 2, 33 },
-            { 3, 31 },
-            { 4, 35 },
-            { 5, 36 },
-            { 6, 37 },
-            { 7, 91 },
-            { 8, 92 },
-        };
 
-        private const string cHome = "\u001b[0;0H";
-        private const string cDefaultColour = "\u001b[0m";
-
-        private static string ColourForNeighbours(int n)
-          => $"\u001b[{sColours[n].ToString()}m";
-
-        public static void Main(params string[] args)
+        public static async Task Main(params string[] args)
         {
             int width = args.Length > 0 ? int.Parse(args[0]) : 40;
             int height = args.Length > 1 ? int.Parse(args[1]) : 20;
@@ -43,31 +26,8 @@ namespace ConwayConsole
 
             while (true)
             {
-                builder.Clear().Append(cHome);
-
-                for (int y = 0; y < board.Height; ++y)
-                {
-                    for (int x = 0; x < board.Width; ++x)
-                    {
-                        if (board.Cell(x,y))
-                        {
-                            builder.Append(ColourForNeighbours(board.Neighbours(x, y)));
-                            builder.Append('O');
-                        }
-                        else
-                        {
-                            builder.Append(' ');
-                        }
-                    }
-
-                    builder.Append(Environment.NewLine);
-                }
-
-                builder.Append(cDefaultColour);
-
-                Console.WriteLine(builder.ToString());
-
-                Thread.Sleep(delay);
+                await Console.Out.WriteLineAsync(board.ToConsoleString(builder));
+                await Task.Delay(delay);
                 board = game.Turn();
             }
         }
