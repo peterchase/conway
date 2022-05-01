65b1ebe6481b5fea5582d3f116493a51157ef91a
Peter Chase <pete@peterchase.me>
2022-05-01 10:48:45 +0100

    Basic test of standard evolution

---
+++ conwaylibtests/StandardEvolutionTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace ConwayLib.Tests
+{
+    [TestFixture]
+    public sealed class StandardEvolutionTests
+    {
+        [TestCase(false, 0, false)]
+        [TestCase(false, 2, false)]
+        [TestCase(false, 3, true)]
+        [TestCase(true, 0, false)]
+        [TestCase(true, 2, true)]
+        [TestCase(true, 3, true)]
+        [TestCase(true, 4, false)]
+        public void GetNextState_ShouldReturnCorrectState(bool curState, int neighbours, bool expected)
+        {
+            Assert.That(StandardEvolution.Instance.GetNextState(curState, neighbours), Is.EqualTo(expected));
+        }
+    }
+}
