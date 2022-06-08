d66520dbc8e0ef60ee7b141fd609633094eb8286
WTWICTStudent1 <WTWICTStudent1@outlook.com>
2022-06-08 09:40:15 +0100

    added the options for window size

--- conwayconsole/CommandLineOptions.cs
+++ conwayconsole/CommandLineOptions.cs
@@ -16,5 +16,17 @@ namespace ConwayConsole
 
         [Option('s', "seed", Required = false, HelpText = "enables you to select the pattern within the random generation")]
         public int? Seed {get; set;} = null;
+
+        [Option("windowWidth", Required = false, HelpText = "The width of the display")]
+        public int? windowWidth {get; set;} = null;
+
+        [Option("windowHeight", Required = false, HelpText = "The height of the display")]
+        public int? windowHeight {get; set;} = null;
+
+        [Option("windowX", Required = false, HelpText = "starting x position of the display")]
+        public int? windowX {get; set;} = null;
+
+        [Option("windowY", Required = false, HelpText = "starting y position of the display")]
+        public int? windowY {get; set;} = null;
     }
 }
