7543da6cb8d5ea10bf7f3b40f25934350788efb8
WTWICTStudent1 <WTWICTStudent1@outlook.com>
2022-06-10 11:35:15 +0100

    Displays board stats

--- conwayconsole/Program.cs
+++ conwayconsole/Program.cs
@@ -94,7 +94,7 @@ namespace ConwayConsole
             TimeSpan elapsed = now.Subtract(lastLoopTime);
             TimeSpan delay = TimeSpan.FromMilliseconds(options.Delay).Subtract(elapsed);
             
-            Console.WriteLine($"\u001b[0;0HThe board size is {board.Width} X {board.Height} the window size {window.Width} X {window.Height}");// \u001b[0;0 is the special code to send the line to the top of the window
+            Console.WriteLine($"\u001b[0;0H({board.Width}x{board.Height}) ({window.Width}x{window.Height}) ({window.X}, {window.Y})");// \u001b[0;0 is the special code to send the line to the top of the window
             if (delay > TimeSpan.Zero)
             {
               await Task.Delay(delay, cts.Token);
