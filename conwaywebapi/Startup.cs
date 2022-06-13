703bdd11bcba5c6b3f85b179510c37b8cf5cbda6
WTWICTStudent3 <WTWICTStudent3@outlook.com>
2022-06-13 15:17:57 +0100

    Exception Filter

--- conwaywebapi/Startup.cs
+++ conwaywebapi/Startup.cs
@@ -39,6 +39,8 @@ namespace ConwayWebApi
 
             services.AddDbContext<ConwayContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
