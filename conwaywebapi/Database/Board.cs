783758b0e2f7123c32923499f0fd6e070a8f494b
WTWICTStudent3 <WTWICTStudent3@outlook.com>
2022-06-14 09:43:59 +0100

    Adjusted Entitiy definitions

--- conwaywebapi/Database/Board.cs
+++ conwaywebapi/Database/Board.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
+
 namespace ConwayWebApi.Database
 {
     public class Board 
     {
-        public long ID { get; set; }
+        public int ID { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
         public string Description { get; set; }
+
+        public ICollection<BoardCell> BoardCells { get; set; }
     }
 }
--- conwaywebapi/Database/BoardCell.cs
+++ conwaywebapi/Database/BoardCell.cs
@@ -2,8 +2,8 @@ namespace ConwayWebApi.Database
 {
     public class BoardCell
     {
-        public long ID {get; set; }
-        public long BoardID { get; set; }
+        public int ID {get; set; }
+        public int BoardID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
     }
