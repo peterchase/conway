31d3be191bb1d47ee98ce9bd78a6eb92ce901c04
WTWICTStudent3 <WTWICTStudent3@outlook.com>
2022-06-15 10:39:56 +0100

    Client Tests

---
+++ conwaywebclienttests/WebClientTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using ConwayWebClient;
+using ConwayLib;
+using System.Threading.Tasks;
+
+namespace ConwayWebClientTests
+{
+    [TestFixture]
+    public class WebClientTests
+    {
+        [SetUp]
+        public void Setup(){
+            ConwayClient.SetupClient("http://localhost:5000/");
+        }
+
+        [Test]
+        public async Task GettingGameState_ShouldGetCorrectBoard()
+        {
+            GameState state = await ConwayClient.GetGameStateAsync(1);
+            CellCoord[] expectedCells = new CellCoord[] {new CellCoord(5,5), new CellCoord(7,8), new CellCoord(2,1)};
+
+            Assert.That(state.SparseData.Length, Is.EqualTo(expectedCells.Length));
+
+            for (int i = 0; i < state.SparseData.Length; i++)
+            {
+                Assert.That(state.SparseData[i], Is.EqualTo(expectedCells[i]));
+            }
+        }
+    }
+}
